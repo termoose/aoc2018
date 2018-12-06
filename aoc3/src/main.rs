342f449bd441ffa2611eb605812b2968299da77a
Ole Andre Birkedal <birkedal85@gmail.com>
2018-12-06 20:10:10 +0100

    part 3a

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -4,6 +4,9 @@ use std::io::BufRead;
 use std::fs::File;
 use regex::Regex;
 
+// #14 @ 662,511: 24x10
+type Patch = (usize, (usize, usize), (usize, usize));
+
 fn str_list(handle: &File) -> Vec<String> {
     let file = BufReader::new(handle);
 
@@ -12,25 +15,43 @@ fn str_list(handle: &File) -> Vec<String> {
         .collect()
 }
 
-fn line_to_tuple(line: &str) -> (i32, (i32, i32), (i32, i32)) {
+fn line_to_tuple(line: &str) -> Patch {
     let caps = Regex::new(r"\#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)$")
         .unwrap();
     let cap = caps.captures(line).unwrap();
 
-    let list: Vec<i32> = cap.iter()
+    let list: Vec<usize> = cap.iter()
         .skip(1)
         .map(|c| c.unwrap().as_str())
-        .map(|s| s.parse::<i32>().unwrap())
+        .map(|s| s.parse::<usize>().unwrap())
         .collect();
 
     (list[0], (list[1], list[2]), (list[3], list[4]))
 }
 
+fn attach(suit: &mut Vec<Vec<u8>>, patch: Patch) {
+    let size = patch.2;
+    let pos = patch.1;
+    for x in 1..(size.0+1) {
+        for y in 1..(size.1+1) {
+            suit[pos.0 + x][pos.1 + y] += 1;
+        }
+    }
+}
+
 fn main() {
     let f = File::open("input.txt").unwrap();
-    let test = "#1 @ 669,271: 17x11";
-    let r = Regex::new(r"\#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)$")
-        .unwrap();
+    let mut suit = vec![vec![0 as u8; 1000]; 1000];
 
-    println!("Matches: {:?}", line_to_tuple(test));
+    // Stitch suit
+    for line in str_list(&f) {
+        attach(&mut suit, line_to_tuple(&line));
+    }
+
+    let reduced: Vec<usize> = suit.iter()
+        .map(|v| {
+            v.iter().filter(|&elem| *elem > 1).count()
+        }).collect();
+
+    println!("Length: {}", reduced.iter().fold(0, |acc, e| acc + e));
 }

a21954a91af02f48a1c841142701863fd68290b7
Ole Andre Birkedal <birkedal@extab.net>
2018-12-10 18:40:19 +0100

    This scan function is not right

--- aoc5/src/main.rs
+++ aoc5/src/main.rs
@@ -27,12 +27,33 @@ fn combine(left: char, right: char) -> Option<String> {
     }
 
     Some([left, right].into_iter().collect())
-}    
+}
+
+fn scan(list: Vec<char>) -> String {
+    let mut result: String = String::new();
+
+    for i in 0..(list.len() - 1) {
+        let first = list[i];
+        let second = list[i+1];
+
+        let combined = combine(first, second);
+
+        match combined {
+            Some(x) => {
+                result += &x;
+            },
+            None => {}
+        }
+    }
+
+    result
+}
 
 fn main() {
     let f = File::open("input.txt").unwrap();
     let file = file_to_string(&f);
 
+    println!("Scan: {}", scan(file.chars().collect()));
     println!("Combine: {:?}", combine('a', 'A'));
     println!("Combine: {:?}", combine('a', 'B'));
 }

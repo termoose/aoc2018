f6e2675b9e1c734dc0f3230e1da881e13441302c
Ole Andre Birkedal <birkedal@extab.net>
2018-12-10 15:51:13 +0100

    Convert guard action to enum

--- aoc4/src/main.rs
+++ aoc4/src/main.rs
@@ -6,12 +6,34 @@ use std::fs::File;
 use regex::Regex;
 use chrono::{NaiveDate, NaiveDateTime};
 
+#[derive(Debug)]
+enum GuardAction {
+    FallAsleep,
+    WakeUp,
+    StartDuty(usize)        
+}
+
 fn str_list(handle: &File) -> Vec<String> {
     let file = BufReader::new(handle);
 
     file.lines()
         .map(|line| line.unwrap().parse::<String>().unwrap())
         .collect()
+}
+
+fn to_action(msg: &str) -> GuardAction {
+    let caps_wakeup = Regex::new(r"Guard \#([0-9]+).+").unwrap();
+
+    if caps_wakeup.is_match(&msg) {
+        let cap = caps_wakeup.captures(msg).unwrap();
+        let id: usize = cap[1].parse::<usize>().unwrap();
+        return GuardAction::StartDuty(id);
+    }
+    else if msg == "wakes up" {
+        return GuardAction::WakeUp;
+    }
+
+    return GuardAction::FallAsleep;
 }
 
 // [1518-11-01 00:05] -> NaiveDateTime
@@ -23,7 +45,6 @@ fn to_date(line: &str) -> (NaiveDateTime, String)
     let list: Vec<_> = cap.iter()
         .skip(1)
         .map(|c| c.unwrap().as_str())
-//        .map(|s| s.parse::<u32>().unwrap())
         .collect();
     
     (NaiveDate::from_ymd(list[0].parse::<i32>().unwrap(),
@@ -38,10 +59,15 @@ fn to_date(line: &str) -> (NaiveDateTime, String)
 fn main() {
     let f = File::open("input.txt").unwrap();
 
-    let lines = str_list(&f);
+    let mut lines: Vec<(NaiveDateTime, String)> = str_list(&f).iter()
+        .map(|l| to_date(&l))
+        .collect();
 
-    for line in lines {
-        let (date, msg) = to_date(&line);
-        println!("Date: {:?} Msg: {}", date, msg);
+    // Sort by the date
+    lines.sort_by(|(d1, _), (d2, _)| d1.cmp(d2));
+
+    for (date, msg) in lines {
+        let action = to_action(&msg);
+        println!("Date: {:?} Action: {:?}", date, action);
     }
 }
